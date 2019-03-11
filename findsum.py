c29cd028cc7aa08acdb10c7e505cf348746a62c4
padmadavid <48451772+padmadavid@users.noreply.github.com>
2019-03-11 23:38:32 +0530

    findsum.py

---
+++ findsum.py
@@ -0,0 +1,10 @@
+#! usr/bin/python
+
+print ('This script adds 2 nos (single/two digits)')
+
+num1 = int(input('Pls enter a number: '))
+num2 = int(input("Pls enter the other number: "))
+
+result = num1 + num2
+
+print ("sum: %02d" % (result))
